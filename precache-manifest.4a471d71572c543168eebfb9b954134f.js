5737d260358eb47133c9331e9cb22dde9af59c80
Steve Griffith <griffis@algonquincollege.com>
2018-10-30 20:09:52 -0400

    Updates

---
+++ precache-manifest.4a471d71572c543168eebfb9b954134f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/production/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8a936e5839e2c369d2bf",
+    "url": "/production/static/js/runtime~main.8a936e58.js"
+  },
+  {
+    "revision": "2e120fd7419ead222573",
+    "url": "/production/static/js/main.2e120fd7.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/production/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "2e120fd7419ead222573",
+    "url": "/production/static/css/main.2a6d9718.chunk.css"
+  },
+  {
+    "revision": "91dededa2f1324e402f8870cb99f9365",
+    "url": "/production/index.html"
+  }
+];
